8472197b185c3d855b2cb37f6edf7f39353f7459
Clifford <kingsleyclifford2016@gmail.com>
2021-11-05 15:49:35 +0000

    this is a new commit

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,12 +1,12 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <img src={props.image} className={classes.photo} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
