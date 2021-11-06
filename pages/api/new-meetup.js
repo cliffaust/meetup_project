08dba8f69f15f0fffe0c21b8e29fe8190d6e9e6b
Clifford <kingsleyclifford2016@gmail.com>
2021-11-06 12:50:55 +0000

    added lines of code

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,3 +1,26 @@
-const handler = (req, res) => {};
+import { MongoClient } from "mongodb";
+
+async const handler = (req, res) => {
+  if (req.method == "POST") {
+    const data = req.body;
+
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://cliffaust:$Gingerpepper2018@cluster0.v3xzu.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db()
+
+    const meetupCollection = db.collection("meetups")
+
+    const result = await meetupCollection.insertOne(data)
+
+    console.log(result)
+
+    client.close()
+
+    res.statusCode(200).json('Meetup has been inserted!')
+  }
+};
 
 export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,14 @@
+import axios from "axios";
 import React from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function newMeetup() {
-  const addMeetupHandler = (enteredMeetup) => {
-    console.log(enteredMeetup);
+  async const addMeetupHandler = (enteredMeetup) => {
+    const response = await axios.post('/api/new-meetup', enteredMeetup)
+
+    const data = await response.json()
+
+    console.log(data)
   };
   return (
     <div>
