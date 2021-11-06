47fd0359a3ccd10cbdbcc1dc8b1ff775bd7dc00b
Clifford <kingsleyclifford2016@gmail.com>
2021-11-06 09:15:36 +0000

    added lines of code

--- pages/[meetup_id]/index.js
+++ pages/[meetup_id]/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import MeetupDetail from "../../components/meetups/MeetupDetail";
+import Meetup from "../../components/meetups/MeetupDetail";
+
+const meetup = {
+  id: 1,
+  title: "Programming is the future",
+  image:
+    "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=772&q=80",
+  address: "343 Danlogo, Italy",
+  description: "This is the first meetup",
+};
 
 function MeetUpDetail() {
-  const meetup = {
-    id: 1,
-    title: "Programming is the future",
-    image:
-      "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=772&q=80",
-    address: "343 Danlogo, Italy",
-    description: "This is the first meetup",
-  };
   return (
     <div>
-      <MeetUpDetail meetup={meetup}></MeetUpDetail>
+      <Meetup meetup={meetup}></Meetup>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,39 @@
 import React from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-function HomePage() {
-  const meetups = [
-    {
-      id: 1,
-      title: "Programming is the future",
-      image:
-        "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=772&q=80",
-      address: "343 Danlogo, Italy",
-      description: "This is the first meetup",
-    },
-    {
-      id: 2,
-      title: "Virtual Reality",
-      image:
-        "https://images.unsplash.com/photo-1534445867742-43195f401b6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=928&q=80",
-      address: "213 James, Italy",
-      description: "This is the second meetup",
-    },
-  ];
+const meetups = [
+  {
+    id: 1,
+    title: "Programming is the future",
+    image:
+      "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=772&q=80",
+    address: "343 Danlogo, Italy",
+    description: "This is the first meetup",
+  },
+  {
+    id: 2,
+    title: "Virtual Reality",
+    image:
+      "https://images.unsplash.com/photo-1534445867742-43195f401b6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=928&q=80",
+    address: "213 James, Italy",
+    description: "This is the second meetup",
+  },
+];
+
+function HomePage(props) {
   return (
     <div>
-      <MeetupList meetups={meetups}></MeetupList>
+      <MeetupList meetups={props.meetups}></MeetupList>
     </div>
   );
 }
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      meetups: meetups,
+    },
+  };
+};
 
 export default HomePage;
