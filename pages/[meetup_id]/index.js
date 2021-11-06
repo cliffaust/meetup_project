209677f42471bcccc5bfe9222d36b2ac408fe04a
Clifford <kingsleyclifford2016@gmail.com>
2021-11-06 14:15:56 +0000

    updated meetup detail file

--- pages/[meetup_id]/index.js
+++ pages/[meetup_id]/index.js
@@ -1,10 +1,10 @@
 import { MongoClient } from "mongodb";
 import Meetup from "../../components/meetups/MeetupDetail";
 
-function MeetUpDetail() {
+function MeetUpDetail(props) {
   return (
     <div>
-      <Meetup meetup={meetup}></Meetup>
+      <Meetup meetup={props.meetup}></Meetup>
     </div>
   );
 }
@@ -26,6 +26,27 @@ export const getStaticPaths = async () => {
     paths: meetup.map((meetup) => ({
       params: { meetup_id: meetup._id.toString() },
     })),
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetup_id;
+  const client = await MongoClient.connect(
+    "mongodb+srv://cliffaust:$Gingerpepper2018@cluster0.v3xzu.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const meetup = await meetupCollection.findOne({ _id: meetupId });
+
+  client.close();
+
+  return {
+    props: {
+      meetup: meetup,
+    },
   };
 };
 
