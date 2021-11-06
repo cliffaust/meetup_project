551763d9cd46206b72777827407afa0f6ea97cea
Clifford <kingsleyclifford2016@gmail.com>
2021-11-06 12:57:02 +0000

    added lines of code

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,25 +1,24 @@
 import { MongoClient } from "mongodb";
 
-async const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method == "POST") {
     const data = req.body;
-
 
     const client = await MongoClient.connect(
       "mongodb+srv://cliffaust:$Gingerpepper2018@cluster0.v3xzu.mongodb.net/meetups?retryWrites=true&w=majority"
     );
 
-    const db = client.db()
+    const db = client.db();
 
-    const meetupCollection = db.collection("meetups")
+    const meetupCollection = db.collection("meetups");
 
-    const result = await meetupCollection.insertOne(data)
+    const result = await meetupCollection.insertOne(data);
 
-    console.log(result)
+    console.log(result);
 
-    client.close()
+    client.close();
 
-    res.statusCode(200).json('Meetup has been inserted!')
+    res.statusCode(200).json("Meetup has been inserted!");
   }
 };
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -3,12 +3,12 @@ import React from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function newMeetup() {
-  async const addMeetupHandler = (enteredMeetup) => {
-    const response = await axios.post('/api/new-meetup', enteredMeetup)
+  const addMeetupHandler = async (enteredMeetup) => {
+    const response = await axios.post("/api/new-meetup", enteredMeetup);
 
-    const data = await response.json()
+    const data = await response.json();
 
-    console.log(data)
+    console.log(data);
   };
   return (
     <div>
