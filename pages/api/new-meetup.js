48f10f19d8d74a7ebe2f5ae75f99148d63e3c43a
Clifford <kingsleyclifford2016@gmail.com>
2021-11-06 13:04:08 +0000

    error handling added to the API

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,19 +4,32 @@ const handler = async (req, res) => {
   if (req.method == "POST") {
     const data = req.body;
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://cliffaust:$Gingerpepper2018@cluster0.v3xzu.mongodb.net/meetups?retryWrites=true&w=majority"
-    );
+    try {
+      try {
+        const client = await MongoClient.connect(
+          "mongodb+srv://cliffaust:$Gingerpepper2018@cluster0.v3xzu.mongodb.net/meetups?retryWrites=true&w=majority"
+        );
 
-    const db = client.db();
+        const db = client.db();
 
-    const meetupCollection = db.collection("meetups");
+        const meetupCollection = db.collection("meetups");
+      } catch (error) {
+        console.log("Database error");
+        console.log(error);
+      }
 
-    const result = await meetupCollection.insertOne(data);
-
-    console.log(result);
-
-    client.close();
+      try {
+        const result = await meetupCollection.insertOne(data);
+        console.log(result);
+      } catch (error) {
+        console.log("Insertion Error");
+        console.log(error);
+      }
+      client.close();
+    } catch (error) {
+      console.log("an error occurred");
+      console.log(error);
+    }
 
     res.statusCode(200).json("Meetup has been inserted!");
   }
